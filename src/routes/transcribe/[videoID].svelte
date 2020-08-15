a496aa122d6dadd850de0a10a27bf3958d0c575f
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-08-16 10:02:19 +1200

    Use url param to select video.

---
+++ src/routes/transcribe/[videoID].svelte
@@ -0,0 +1,98 @@
+<script context="module">
+	export function preload(page) {
+		return { videoID: page.params.videoID }
+	}
+</script>
+
+<script>
+	import { Howl } from 'howler';
+
+	import { stores } from '@sapper/app';
+	const { page } = stores();
+	export let videoID
+	console.log("videoID: " + videoID)
+
+	const ytPrefix = 'https://www.youtube.com/watch?v='
+	let url = ytPrefix + videoID
+	$: videoID = url.startsWith(ytPrefix)? url.replace(ytPrefix, ""): "";
+	$: thumbnailURL = "http://img.youtube.com/vi/" + videoID + "/0.jpg" ;
+
+	let audioChosen = false
+
+	async function getYTThumbnail(videoID) {
+		if (typeof fetch !== 'undefined') {
+			const response = await fetch("getYTAsset/thumbnail/" + videoID, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/octet-stream'
+				},
+			})
+			const image = await response.blob()
+
+			let data = await image.arrayBuffer()
+			let base64String = btoa(String.fromCharCode(...new Uint8Array(data)));
+			
+			return 'data:image/jpeg;base64,' + base64String;
+		}
+	}
+
+	async function getYTTitle(videoID) {
+		if (typeof fetch !== 'undefined') {
+			const response = await fetch("getYTAsset/title/" + videoID, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+			})
+
+			const json = await response.json()
+			return json.title;
+		}
+	}
+
+	async function getYTAudio() {
+		let howlPromise = new Promise((resolve, reject) => {
+			let sound;
+			sound = new Howl({
+				src: ['getYTAudio/' + videoID],
+				format: 'mp3',
+				onload: () => {
+					resolve(sound)
+				},
+			});
+		})
+
+		return howlPromise;
+	}
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+{#if audioChosen}
+	{#await getYTAudio()} 
+		<h1>Waiting on audio processing . . .</h1>
+	{:then audioPlayer}
+		<button on:click={()=>audioPlayer.play()}>Play</button>
+	{/await}
+{:else}
+	<label>
+		YouTube Link
+		<input bind:value={url}>
+	</label>
+
+	<button on:click={() => {audioChosen = true}}>
+		Prepare Audio
+	</button>
+
+	<hr>
+
+	{#await getYTTitle(videoID) then videoTitle}
+		<h1>{videoTitle}</h1>
+	{/await}
+
+	{#await getYTThumbnail(videoID) then thumbnail}
+		<img src={thumbnail} alt="YouTube Thumbnail">
+	{/await}
+{/if}
