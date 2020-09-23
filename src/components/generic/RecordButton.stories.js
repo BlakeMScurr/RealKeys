5c62d0e3f832d2a0b9983f710bfc70293754fbdb
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-09-24 11:44:42 +1200

    Add recording button.

---
+++ src/components/generic/RecordButton.stories.js
@@ -0,0 +1,8 @@
+import RecordButton from './RecordButton.svelte';
+export default {
+  title: 'RecordButton',
+};
+
+export const Default = () => ({
+  Component: RecordButton,
+});
---
+++ src/components/generic/RecordButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    let dispatch = createEventDispatcher();
+    let recording = false
+
+    function start () {
+        dispatch("startRecording")
+        recording = true
+    }
+
+    function stop () {
+        dispatch("stopRecording")
+        recording = false
+    }
+</script>
+
+<style>
+    .stop {
+        background-color: red;
+        color:white;
+    }
+</style>
+
+{#if recording}
+    <button class="stop" on:click={stop}>Stop Recording</button>
+{:else}
+    <button class="start" on:click={start}>Record</button>
+{/if}
