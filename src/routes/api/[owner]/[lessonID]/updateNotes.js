c11a17c97ba802ad74146a3be20e60f3cfe19db1
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-10-01 14:52:14 +1300

    Remove beat calculation.

--- src/routes/api/[owner]/[lessonID]/updateNotes.js
+++ src/routes/api/[owner]/[lessonID]/updateNotes.js
@@ -1,6 +1,4 @@
-import { calculateBars } from "../../../../backend/beatcalculation/barCalculation.js"
 import { NewPool } from "../../../../backend/db/client.js"
-import { downloadYouTubeVideo } from "../../../../backend/youtube-dl.js"
 var prep = require('pg-prepared')
 
 export function post(request, response) {
