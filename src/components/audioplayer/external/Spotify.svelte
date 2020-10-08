bdb2de735a89383d4b4de510d2ea4ca4dbbedf82
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-10-09 11:57:47 +1300

    Add token.

--- src/components/audioplayer/external/Spotify.svelte
+++ src/components/audioplayer/external/Spotify.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     export let track:string;
+
+    // TODO: put somewhere much safer
+    const token = "BQAVinwLW-6-mpof8PnWiLrfA_W9pgAJ2e96GPEXhmdrGKVPACebkquyxYTV0nUIKwwa_EUApvvMPL3JwjhcGniWNySJQ1q_vku-STz-d3kcz-XAdYkIBdqD3hTMJZnlpws8rae4xhtTScp4uja1UMBoMRR2oI0OvaY"
 </script>
 
 <iframe title="spotify"
@@ -10,3 +13,5 @@
     allowtransparency="true"
     allow="encrypted-media">
 </iframe>
+
+
