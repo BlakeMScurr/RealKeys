683594787d0ad50b61dc26be67875a0c21df1527
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-10-10 17:09:09 +1300

    basic callback.

--- src/routes/callback.svelte
+++ src/routes/callback.svelte
@@ -1,1 +1,13 @@
-Login successful
+<!-- TODO: rename file to spotify callback -->
+<script lang="ts">
+    import queryString from "query-string";
+
+    let parsed = {};
+    if (typeof window !== 'undefined') {
+        parsed = queryString.parse(window.location.search);
+    }
+</script>
+
+Login happened
+
+{parsed}
