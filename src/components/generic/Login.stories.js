0b9c56bd7ea013d58ebccabf3d6e0dc60952f1a5
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-10-10 16:29:50 +1300

    Redirect to spotify authorization.

---
+++ src/components/generic/Login.stories.js
@@ -0,0 +1,8 @@
+import Login from './Login.svelte';
+export default {
+  title: 'Login',
+};
+
+export const Default = () => ({
+  Component: Login,
+});
---
+++ src/components/generic/Login.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { goto } from '@sapper/app';
+
+    function handleClick() {
+        var scopes = 'user-read-private user-read-email';
+        var redirect_uri = 'http://realkeys.co/callback'
+        goto('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + '9985cfc25fad4e3e82794d87f23823ef' +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent(redirect_uri));
+        }
+</script>
+
+<button on:click={handleClick}>
+    Log in with Spotify
+</button>
