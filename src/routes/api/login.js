b0a1cec4e8b4eab91f1cbc3621e26f33491ab3c2
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-10-11 16:12:46 +1300

    Get token working with request (rather than fetch).

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -1,25 +1,39 @@
 import { spotifyRedirectURI, spotifyAuthorization } from "../../lib/util"
 const fetch = require("node-fetch");
+const request = require("request");
 var FormData = require('form-data');
 
-export function post(request, response) {
-    let { code } = request.body;
+export function post(req, res) {
+    let { code } = req.body;
     console.log(code)
-    fetch("https://accounts.spotify.com/api/token", {
+
+    var postQuery = 'grant_type=authorization_code&code='+code+"&redirect_uri="+spotifyRedirectURI();
+    request({
+        url: "https://accounts.spotify.com/api/token",
         method: "POST",
         headers: {
-            'Authorization': spotifyAuthorization(),
-            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-            // 'Content-Type':'application/json',
+          'Authorization': spotifyAuthorization(),
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': postQuery.length
         },
-        body: new FormData({
-            grant_type: "authorization_code",
-            code: code,
-            redirect_uri: spotifyRedirectURI(),
-        })
-    }).then(res => {
-        console.log(res)
-    }).catch((e)=>{
-        console.log("failure", e)
-    })
+        body: postQuery
+      }, function (error, response, data){
+        //send the access token back to client
+        res.end(data);
+      });    
+
+
+    // fetch("https://accounts.spotify.com/api/token", {
+    //     method: "POST",
+    //     headers: {
+    //         'Authorization': spotifyAuthorization(),
+    //         'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+    //         'Content-Length': postQuery.length,
+    //     },
+    //     body: postQuery,
+    // }).then(res => {
+    //     console.log(res)
+    // }).catch((e)=>{
+    //     console.log("failure", e)
+    // })
 }
