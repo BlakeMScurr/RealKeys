7433ef10c805c810b8b9ea70a8d9104f7bbcf975
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2020-10-24 16:16:35 +1300

    Clean up GL api.

--- src/components/pianoroll/roll/GL.ts
+++ src/components/pianoroll/roll/GL.ts
@@ -15,3 +15,168 @@ uniform vec4 uColor;
 void main() {
 gl_FragColor = uColor;
 }`
+
+
+function square(x, y, width, height) {
+    return [
+        // First triangle
+        x, y,
+        x + width, y,
+        x, y + height,
+        
+        // Second triangle
+        x + width, y + height,
+        x + width, y,
+        x, y + height,
+    ]
+}
+
+export function initProgram(gl: WebGLRenderingContext) {
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vs, vertexCode);
+    gl.compileShader(vs);
+
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fragmentCode);
+    gl.compileShader(fs);
+
+    let program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+
+    if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) console.log(gl.getShaderInfoLog(vs));
+    if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) console.log(gl.getShaderInfoLog(fs));
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) console.log(gl.getProgramInfoLog(program));
+
+    return new drawer(gl, program)
+}
+
+class drawer {
+    gl: WebGLRenderingContext;
+    program: WebGLProgram;
+    constructor(gl: WebGLRenderingContext, program: WebGLProgram) {
+        this.gl = gl
+        this.program = program
+    }
+
+    // ROLL SPECIFIC METHODS
+    // -------------------------------
+
+    drawNotes(keys, notes) {
+        let noteToX = new Map();
+        let width = 2 / keys.length
+    
+        keys.forEach((key, i) => {
+            noteToX.set(key.string(), width * i - 1)
+        })
+    
+        var points = []
+        notes.forEach((note) => {
+            let x = noteToX.get(note.note.string())
+            let sqr = square(x, note.start * 2 - 1, width, (note.end - note.start) * 2)
+            points.push(...sqr)
+        })
+    
+        var vertices = new Float32Array(points);
+        this.drawTriangles(new glColour(0.3984375, 0.4921875, 0.828125, 1.0), vertices)
+    }
+    
+    drawBars(bars) {
+        var points = []
+        // position just records widths of previous bars, since they're widths not positions in the song
+        let position = 0
+        bars.forEach((bar) => {
+            position += bar
+            points.push(
+                -1, position * 2 - 1,
+                1, position * 2 - 1,
+            )
+        })
+        var vertices = new Float32Array(points);
+        this.drawLines(new glColour(1, 1, 1, 1.0), vertices)
+    }
+    
+    drawDividers(keys) {
+        let width = 2 / keys.length
+        var points = []
+        for (var i = 0; i < keys.length - 1; i++) {
+            if (!keys[i].abstract.accidental && !keys[i+1].abstract.accidental) {
+                points.push(
+                    -1 + (i + 1) * width, -1,
+                    -1 + (i + 1) * width, 1,
+                )
+            }
+        }
+        var vertices = new Float32Array(points);
+        this.drawLines(new glColour(1, 1, 1, 1.0), vertices)
+    }
+    
+    drawBlackkeys(keys) {
+        let width = 2 / keys.length
+        var points = []
+        keys.forEach((key, i)=> {
+            if (key.abstract.accidental) {
+                points.push(...square(i * width -1, -1, width, 2))
+            }
+        })
+        var vertices = new Float32Array(points);
+        this.drawTriangles(new glColour(0.3515625, 0.3515625, 0.3515625, 1.0), vertices)
+    }
+
+    setBackground(canvas) {
+        this.gl.viewport(0, 0, canvas.width, canvas.height);
+        this.gl.clearColor(0.546875, 0.546875, 0.546875, 1);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    }
+    
+    // GENERAL UTILITIES
+    // -------------------------------
+
+    drawLines(colour: glColour, vertices: Float32Array) {
+        this.drawStuff(colour, vertices, this.gl.LINES)
+    }
+
+    drawTriangles (colour: glColour, vertices: Float32Array) {
+        this.drawStuff(colour, vertices, this.gl.TRIANGLES)
+    }
+    
+    drawStuff (colour: glColour, vertices: Float32Array, primitive) {
+        let vbuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vbuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
+    
+        let itemSize = 2;
+        let numItems = vertices.length / itemSize;
+    
+        this.gl.useProgram(this.program);
+    
+        let uColor = this.gl.getUniformLocation(this.program, "uColor");
+        this.gl.uniform4fv(uColor, colour.array());
+    
+        let aVertexPosition = this.gl.getAttribLocation(this.program, "aVertexPosition");
+        this.gl.enableVertexAttribArray(aVertexPosition);
+        this.gl.vertexAttribPointer(aVertexPosition, itemSize, this.gl.FLOAT, false, 0, 0);
+    
+        this.gl.drawArrays(primitive, 0, numItems);
+    }
+}
+
+class glColour {
+    r: number;
+    g: number;
+    b: number;
+    opacity: number;
+
+    constructor(r, g, b, opacity) {
+        this.r = r
+        this.g = g
+        this.b = b
+        this.opacity = opacity
+    }
+
+    // formats colour in a format usable by gl.uniform4fv
+    array() {
+        return [this.r, this.g, this.b, this.opacity]
+    }
+}
--- src/components/pianoroll/roll/GLRoll.svelte
+++ src/components/pianoroll/roll/GLRoll.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import type { Note } from "../../../lib/music/theory/notes";
-    import { vertexCode, fragmentCode } from "./GL"
+    import { setBackground, initProgram, drawBlackkeys, drawDividers, drawBars, drawNotes } from "./GL"
     import { TimedNote, TimedNotes } from "../../../lib/music/timed/timed";
     import type { Bars } from "../pianoroll";
 
@@ -46,160 +46,14 @@
             return;
         }
 
-        setBackground(gl, canvas)
-        let program = initProgram(gl)
-        drawBlackkeys(gl, program)
-        drawDividers(gl, program)
-        drawBars(gl, program)
-        drawNotes(gl, program)
-    }
+        
+        let drawer = initProgram(gl)
 
-    function square(x, y, width, height) {
-        return [
-            // First triangle
-            x, y,
-            x + width, y,
-            x, y + height,
-            
-            // Second triangle
-            x + width, y + height,
-            x + width, y,
-            x, y + height,
-        ]
-    }
-
-    function drawNotes(gl, program) {
-        let noteToX = new Map();
-        let width = 2 / keys.length
-
-        keys.forEach((key, i) => {
-            noteToX.set(key.string(), width * i - 1)
-        })
-
-        var points = []
-        zoomedNotes.forEach((note) => {
-            let x = noteToX.get(note.note.string())
-            let sqr = square(x, note.start * 2 - 1, width, (note.end - note.start) * 2)
-            points.push(...sqr)
-        })
-
-        var vertices = new Float32Array(points);
-        drawTriangles(gl, program, new glColour(0.3984375, 0.4921875, 0.828125, 1.0), vertices)
-    }
-
-    function drawBars(gl, program) {
-        var points = []
-        // position just records widths of previous bars, since they're widths not positions in the song
-        let position = 0
-        zoomedBars.forEach((bar) => {
-            position += bar
-            points.push(
-                -1, position * 2 - 1,
-                1, position * 2 - 1,
-            )
-        })
-        var vertices = new Float32Array(points);
-        drawLines(gl, program, new glColour(1, 1, 1, 1.0), vertices)
-    }
-
-    function drawDividers(gl, program) {
-        let width = 2 / keys.length
-        var points = []
-        for (var i = 0; i < keys.length - 1; i++) {
-            if (!keys[i].abstract.accidental && !keys[i+1].abstract.accidental) {
-                points.push(
-                    -1 + (i + 1) * width, -1,
-                    -1 + (i + 1) * width, 1,
-                )
-            }
-        }
-        var vertices = new Float32Array(points);
-        drawLines(gl, program, new glColour(1, 1, 1, 1.0), vertices)
-    }
-
-    function drawBlackkeys(gl, program) {
-        let width = 2 / keys.length
-        var points = []
-        keys.forEach((key, i)=> {
-            if (key.abstract.accidental) {
-                points.push(...square(i * width -1, -1, width, 2))
-            }
-        })
-        var vertices = new Float32Array(points);
-        drawTriangles(gl, program, new glColour(0.3515625, 0.3515625, 0.3515625, 1.0), vertices)
-    }
-
-    function drawLines (gl, program, colour: glColour, vertices: Float32Array) {
-        drawStuff(gl, program, colour, vertices, gl.LINES)
-    }
-    function drawTriangles (gl, program, colour: glColour, vertices: Float32Array) {
-        drawStuff(gl, program, colour, vertices, gl.TRIANGLES)
-    }
-
-    function drawStuff (gl, program, colour: glColour, vertices: Float32Array, primitive) {
-        let vbuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-        let itemSize = 2;
-        let numItems = vertices.length / itemSize;
-
-        gl.useProgram(program);
-
-        let uColor = gl.getUniformLocation(program, "uColor");
-        gl.uniform4fv(uColor, colour.array());
-
-        let aVertexPosition = gl.getAttribLocation(program, "aVertexPosition");
-        gl.enableVertexAttribArray(aVertexPosition);
-        gl.vertexAttribPointer(aVertexPosition, itemSize, gl.FLOAT, false, 0, 0);
-
-        gl.drawArrays(primitive, 0, numItems);
-    }
-
-    function setBackground(gl, canvas) {
-        gl.viewport(0, 0, canvas.width, canvas.height);
-        gl.clearColor(0.546875, 0.546875, 0.546875, 1);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-    }
-
-    function initProgram(gl) {
-        var vs = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vs, vertexCode);
-        gl.compileShader(vs);
-
-        var fs = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fs, fragmentCode);
-        gl.compileShader(fs);
-
-        let program = gl.createProgram();
-        gl.attachShader(program, vs);
-        gl.attachShader(program, fs);
-        gl.linkProgram(program);
-
-        if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) console.log(gl.getShaderInfoLog(vs));
-        if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) console.log(gl.getShaderInfoLog(fs));
-        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) console.log(gl.getProgramInfoLog(program));
-
-        return program
-    }
-
-    class glColour {
-        r: number;
-        g: number;
-        b: number;
-        opacity: number;
-
-        constructor(r, g, b, opacity) {
-            this.r = r
-            this.g = g
-            this.b = b
-            this.opacity = opacity
-        }
-
-        // formats colour in a format usable by gl.uniform4fv
-        array() {
-            return [this.r, this.g, this.b, this.opacity]
-        }
+        drawer.setBackground(canvas)
+        drawer.drawBlackkeys(keys)
+        drawer.drawDividers(keys)
+        drawer.drawBars(zoomedBars)
+        drawer.drawNotes(keys, zoomedNotes)
     }
 </script>
 
