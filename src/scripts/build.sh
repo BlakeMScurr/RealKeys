3382a588bb35f663831aa3d69175ea8abf048e16
blakemscurr <blakemscurr@gmail.com>
2021-02-25 13:51:40 +1300

    More build fix

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,5 +1,6 @@
+cd ../..
 git pull origin master
 npm run build
 ps -o ppid= -C "node __sapper__/build" | xargs kill
 pgrep -f "node __sapper__/build/" | xargs kill
-./run.sh &
+src/scripts/run.sh &
