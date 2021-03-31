8d0d5accdfef1f309f78114f37060963f43ceacf
blakemscurr <blakemscurr@gmail.com>
2021-04-01 11:31:13 +1300

    Remove unneeded pages.

--- src/routes/api/course.ts
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-import { midiLocation } from '../../lib/util'
-
-export function get(request, response) {
-    let p = path.resolve(midiLocation + request.query.name + "/tutorial.txt")
-    response.sendFile(p)
-}
