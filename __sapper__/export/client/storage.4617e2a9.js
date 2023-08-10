1ec9f3eff2426ffb2a6d4a7284217837fddda13a
blakemscurr <blakemscurr@gmail.com>
2023-08-10 20:40:53 +1200

    Remove unnecessary stuff and add built site.

---
+++ __sapper__/export/client/storage.4617e2a9.js
@@ -0,0 +1,1 @@
+function t(){return localStorage.getItem("settings")}function e(t){localStorage.setItem("settings",t)}var o;!function(t){t.midi="MIDI Keyboard",t.qwerty="QWERTY Keyboard",t.touch="Touch/Click"}(o||(o={}));export{t as g,o as i,e as s};
--- src/lib/util.test.ts
+++
@@ -1,7 +0,0 @@
-import { midiPathToName } from "./util"
-
-test("midiPathToName", ()=>{
-    expect(midiPathToName("/K/K/katy_perry-i_kissed_a_girl.mid")).toBe("Katy Perry - I Kissed A Girl")
-    expect(midiPathToName("/H/H/HereComesTheSun.mid")).toBe("Here Comes The Sun")
-    expect(midiPathToName("/A/A/ADayInTheLife2.mid")).toBe("A Day In The Life2")
-})
